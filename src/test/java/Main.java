64291aa85945d9a348230d43779da86fcc79973e
rhapsody1290 <369201191@qq.com>
2016-08-16 17:17:51 +0800

    update为Hibernate操作数据库模版

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -10,31 +10,9 @@ import org.hibernate.cfg.Configuration;
  */
 public class Main {
     public static void main(String[] args){
-        addEmployee();
+        updateEmployee();
     }
-
-    public static void deleteEmployee() {
-        //获取一个会话
-        Session session = MySessionFactory.getInstatnce().openSession();
-        Transaction transaction = session.beginTransaction();
-        Employee employee = (Employee) session.load(Employee.class,2);
-        session.delete(employee);
-        transaction.commit();
-        session.close();
-    }
-
-    public static void updateEmployee() {
-        //获取一个会话
-        Session session = MySessionFactory.getInstatnce().openSession();
-        Transaction transaction = session.beginTransaction();
-        //修改用户1、获得要修改的对象2、修改
-        //load是通过主键属性，获取该对象实例
-        Employee employee = (Employee) session.load(Employee.class,2);//产生select .. where id = 2
-        employee.setName("钱明");//这句话会产生update语句
-        transaction.commit();
-        session.close();
-    }
-
+    //快速入门
     public static void addEmployee() {
         //1、创建configuration，该对象用于读取hibernate.ctf.xml，并完成初始化
         Configuration configuration = new Configuration().configure();
@@ -56,4 +34,46 @@ public class Main {
         transaction.commit();
         session.close();
     }
+
+    //模版
+    public static void updateEmployee() {
+        //获取一个会话
+        Session session = MySessionFactory.getInstatnce().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            //do...
+            //修改用户1、获得要修改的对象2、修改
+            //load是通过主键属性，获取该对象实例
+            Employee employee = (Employee) session.load(Employee.class,3);//产生select .. where id = 2
+            employee.setName("钱明");//这句话会产生update语句
+            //出现异常
+            //int i = 9/0;
+            transaction.commit();
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            throw new RuntimeException(e.getMessage());
+        }finally {
+            //关闭session
+            if(session != null && session.isOpen()){
+                session.close();
+            }
+        }
+    }
+
+    public static void deleteEmployee() {
+        //获取一个会话
+        Session session = MySessionFactory.getInstatnce().openSession();
+        Transaction transaction = session.beginTransaction();
+        Employee employee = (Employee) session.load(Employee.class,2);
+        session.delete(employee);
+        transaction.commit();
+        session.close();
+    }
+
+
+
+
 }
